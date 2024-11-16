0770364147b3ec1908fb3d64e27cbb7f6df4d536
Joao_Otavio <jotavio250@gmail.com>
2024-11-16 16:50:57 -0300

    Galeria

--- dashboard/src/components/Gallery/index.tsx
+++ dashboard/src/components/Gallery/index.tsx
@@ -4,7 +4,16 @@ import './style.css';
 const Gallery = () => {
   return (
     <>
-      <div className="Container">
+      <div className="Container_video">
+        <div className="Video">
+          <video autoPlay loop muted playsInline className="background-clip">
+            <source src={require('../../assets/Video.mp4')} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+
+      <div className="Container_img">
           <div className="Foto">
             <div className="IMG">
               <img src={require('../../assets/foto1.jpg')} alt="" />
